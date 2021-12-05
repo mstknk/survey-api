96ae00e07188eac6945fbd0adbcc8ed89b5feb07
mesut sali <mesutkonak89@gmail.com>
2021-12-05 23:22:43 +0100

    initial commit

---
+++ src/main/java/com/surveyapi/SurveyApiApplication.java
@@ -0,0 +1,14 @@
+package com.surveyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyApiApplication.class, args);
+	}
+
+
+}
